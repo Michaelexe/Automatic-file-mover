f0e972bed95dd170d2e5d5cf3dcfc29c13fdba2d
Michaelexe <michaelispahani@gmail.com>
2020-07-16 22:03:12 +0430

    added music and videos

--- automove.py
+++ automove.py
@@ -4,7 +4,7 @@ import shutil
 downloads = "/home/michaelexe/Downloads"
 pictures = "/home/michaelexe/Pictures"
 videos = "/home/michaelexe/Videos"
-
+music = "/home/michaelexe/Music"
 os.chdir(downloads)
 
 for files in os.listdir():
@@ -12,3 +12,9 @@ for files in os.listdir():
 
 	if file_ext == ".jpg" or file_ext == ".jpeg" or file_ext == ".png":
 		shutil.move(files, pictures)
+
+	if file_ext == ".mp4":
+		shutil.move(files, videos)
+
+	if file_ext == ".mp3":
+		shutil.move(files, music)
