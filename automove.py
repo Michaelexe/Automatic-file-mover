b2cd91ddee2d58c765d9e745faa7058bf2f87967
Michaelexe <michaelispahani@gmail.com>
2021-08-06 23:17:48 +0430

    added print statements lol

--- automove.py
+++ automove.py
@@ -12,9 +12,12 @@ for files in os.listdir():
 
 	if file_ext == ".jpg" or file_ext == ".jpeg" or file_ext == ".png":
 		shutil.move(files, pictures)
+		print(files + ' has been moved to your pictures folder')
 
 	if file_ext == ".mp4":
 		shutil.move(files, videos)
+		print(files + ' has been moved to your videos folder')
 
 	if file_ext == ".mp3":
 		shutil.move(files, music)
+		print(files + ' has been moved to your music folder')
